c270cde5a887cd365d3931599071f52e10115aaa
trendhubing <mahamrizwan.360958@gmail.com>
2026-02-10 20:44:33 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME="trend-hubing-v1";
 const urlsToCache=["/","/index.html","/home.html","/home/styles/style.css","/home/javascripts/javascript.js","/home/styles/homestyle.css","/home/javascripts/homejavascript.js",
-"/home/fashion.html","/home/color.html",
+"/home/fashion.html","/home/color.html","/home/jewelry.html",
 "/home/food.html","/home/spicyfood.html","/home/dessert.html",
-"/home/car.html",
-"/home/travel.html",
-"/home/game.html","/home/multigame.html",
-"/home/bts.html",
-"/home/kpop.html",
+"/home/car.html","/home/sportcar.html","/home/electriccar.html",
+"/home/travel.html","/home/adventure.html","/home/ride.html",
+"/home/game.html","/home/multigame.html","/home/pcgame.html",
+"/home/bts.html","/home/btsbiography.html","/home/btssong.html",
+"/home/kpop.html","/home/bp.html",
 "/home/movie.html","/home/cartoonmovie.html","/home/comedymovie.html",
-"/home/anime.html","/home/animatedcartoon.html",
-"/home/sport.html",];
+"/home/anime.html","/home/animatedcartoon.html","/home/comedyanime.html",
+"/home/sport.html","/home/indoor.html","/home/outdoor.html",];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>{
 return cache.addAll(urlsToCache);}));});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>{
@@ -33,4 +33,5 @@ self.addEventListener("fetch", event => {
       });
     })
   );
+
 });
